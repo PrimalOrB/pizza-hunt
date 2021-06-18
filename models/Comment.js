88d6c40d32bfb8f8939f5af336e23707065e8f94
Andrew Ogilvie <primalorb@gmail.com>
2021-06-18 15:15:39 -0400

    add model validation

--- models/Comment.js
+++ models/Comment.js
@@ -1,36 +1,43 @@
 const { Schema, model, Types } = require( 'mongoose' );
 const dateFormat = require( '../utils/dateFormat' );
 
-const ReplySchema = new Schema( 
+const ReplySchema = new Schema(
     {
-        replyBody: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        writtenBy: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat( createdAtVal )
-        },
+      replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      replyBody: {
+        type: String,
+        required: true
+      },
+      writtenBy: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat( createdAtVal )
+      }
     },
     {
-        toJSON: {
-            getters: true
-        }
+      toJSON: {
+        getters: true
+      }
     }
- )
+  );
 
 
 const CommentSchema = new Schema( 
     { 
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
